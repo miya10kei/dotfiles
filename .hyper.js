e8728f911df4d1cf6d3df5cddf33d1ed4c85cfa7
miya10kei <miya10kei@gmail.com>
2021-05-23 14:46:15 +0900

    :pencil2: Change font from Souce Code Pro to DaddyTimeMono Nerd Font Mono

--- .hyper.js
+++ .hyper.js
@@ -13,8 +13,7 @@ module.exports = {
 
     // font family with optional fallbacks
     // fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
-    fontFamily: '"Source Code Pro for Powerline","Noto Color Emoji", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
-
+    fontFamily: '"DaddyTimeMono Nerd Font Mono","Noto Color Emoji", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
