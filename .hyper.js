99d81c5b9f6f33768c3b1af9405a625c968a83fe
miya10kei <miya10kei@gmail.com>
2021-07-05 14:00:12 +0900

    Modify cursorShape

--- .hyper.js
+++ .hyper.js
@@ -34,7 +34,7 @@ module.exports = {
     cursorAccentColor: '#000',
 
     // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
-    cursorShape: 'UNDERLINE',
+    cursorShape: 'BLOCK',
 
     // set to `true` (without backticks and without quotes) for blinking cursor
     cursorBlink: false,
