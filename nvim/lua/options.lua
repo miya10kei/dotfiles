3d9922496faae0467328ff5efec27e00ea8ce208
miya10kei <miya10kei@gmail.com>
2023-04-03 21:30:37 +0900

    Fix bug

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -21,18 +21,3 @@ local options = {
 for k, v in pairs(options) do
     vim.opt[k] = v
 end
-
-if vim.env.TMUX then
-    vim.g.clipboard = {
-        name = 'tmux',
-        copy = {
-            ["+"] = {'tmux', 'load-buffer', '-w', '-'},
-            ["*"] = {'tmux', 'load-buffer', '-w', '-'},
-        },
-        paste = {
-            ["+"] = {'tmux', 'save-buffer', '-'},
-            ["*"] = {'tmux', 'save-buffer', '-'},
-        },
-        cache_enabled = false,
-    }
-end
