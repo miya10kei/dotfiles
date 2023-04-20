15cd2a4224e03e69df659bbb299a8d50b5763479
miya10kei <miya10kei@gmail.com>
2023-04-20 17:03:54 +0900

    Remove denops daemon

--- Makefile.d/jobs.mk
+++ Makefile.d/jobs.mk
@@ -1,11 +1,6 @@
 .PHONY: start-bg-job
 start-bg-job: \
-	start-denops \
 	start-gopls
-
-.PHONY: start-denops
-start-denops:
-	nohup deno run -A --no-lock $(HOME)/.local/share/nvim/lazy/denops.vim/denops/@denops-private/cli.ts > /dev/null 2>&1 &
 
 .PHONY: start-gopls
 start-gopls:
