8526971b45390986ffa6d8ad7d57bfa08838818d
miya10kei <miya10kei@gmail.com>
2023-04-20 17:06:54 +0900

    Remove jj map

--- nvim/lua/terminal.lua
+++ nvim/lua/terminal.lua
@@ -2,7 +2,6 @@ local keymap = vim.keymap.set
 
 keymap('n', '<F12>', '<CMD>belowright new<CR><CMD>terminal<CR>')
 keymap('t', '<ESC>', '<C-\\><C-n>')
-keymap('t', 'jj', '<C-\\><C-n>')
 
 vim.api.nvim_create_augroup('terminal_config', {})
 vim.api.nvim_create_autocmd('TermOpen', {
