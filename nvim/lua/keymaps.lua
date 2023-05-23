4b1b36f082813247f8262cbf3902a7e17a23fbe6
miya10kei <miya10kei@gmail.com>
2023-05-23 15:49:43 +0900

    Add keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,5 +1,4 @@
-local keymap = vim.keymap.set
-local fn = vim.fn
+local keymap = vim.keymap.set local fn = vim.fn
 
 vim.g.mapleader = ' '
 
@@ -17,6 +16,7 @@ keymap('n', 'q', '<NOP>')
 keymap('n', '<C-n>', function()
     return vim.o.number == true and ':<C-u>set nonumber<CR>' or ':<C-u>set number<CR>'
 end, { expr = true, silent = true })
+keymap('n', '<SPACE><SPACE>', "\"zyiw:let @/ = '\\<' . @z . '\\>'<CR>:set hlsearch<CR>")
 
 -- ddc
 keymap('i', '<TAB>', function()
