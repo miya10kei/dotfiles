8f5fb6f46dae2e282c0377ff974458e97912c745
miya10kei <miya10kei@gmail.com>
2023-05-29 17:29:23 +0900

    Polish cd command

--- zshrc.d/aliases.zsh
+++ zshrc.d/aliases.zsh
@@ -13,10 +13,19 @@ function alias_if_exists() {
     fi
 }
 
-alias_if_exists 'cdg'  'cd $(ghq list --full-path | fzf)' 'ghq'
+function _fzf_change_directory() {
+  selected=$(eval $1 | fzf)
+  if [ -n "$selected" ]; then
+    base_dir=$(eval $2)
+    cd "$base_dir/$selected"
+  fi
+}
+
+alias_if_exists 'cdc'  '_fzf_change_directory "ls -1D" "pwd"'
+alias_if_exists 'cdg'  '_fzf_change_directory "ghq list" "ghq root"' 'ghq'
 alias_if_exists 'cdot' 'cd $HOME/.dotfiles'
 alias_if_exists 'cdr'  'cd -'
-alias_if_exists 'cdw'  'cd $HOME/dev/workspaces'
+alias_if_exists 'cdw'  '_fzf_change_directory "ls -1D $HOME/dev/workspaces" "echo $HOME/dev/workspaces"'
 alias_if_exists 'dk'   'docker'          'docker'
 alias_if_exists 'g'    'git'             'git'
 alias_if_exists 'll'   'exa -al'         'exa' 'ls -la'
